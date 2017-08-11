64a1c68ac8da59099c79eb2f572323fea6715f7d
Dane Filipczak <danefilipczak@Danes-MacBook-Pro.local>
2017-08-11 14:16:41 -0400

    got a basic search box

--- js/app.js
+++ js/app.js
@@ -43,6 +43,10 @@ function ViewModel() {
     	}
     }
 
+
+    this.setCurrentSpecimen = function(spec){
+    	self.currentSpecimen(spec);
+    }
     //this.picSrc = ko.observable("")
 
     
@@ -54,6 +58,24 @@ function ViewModel() {
 app = new ViewModel()
 ko.applyBindings(app);
 
+
+//helper function 
+function filterList() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}
 
 
 
